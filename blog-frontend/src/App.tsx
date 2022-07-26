8868128b1a16860317ecc14e6ffff58293a9f78b
Javier Guerra <javier.guerra1001@gmail.com>
2022-07-26 14:13:23 -0700

    app:(imported footer)

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Footer from './components/Footer';
 
 function App() {
-
     return (
     <main>
       <Footer/>
    </main>
   );
-}
+};
 
 export default App;
