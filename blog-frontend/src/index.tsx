531266e496a8274f832933ea53d7897b895ee8ce
tdavis0828 <davis.t828@gmail.com>
2022-07-27 15:48:03 -0700

    setting up react router, redux store, styling, and ui build out

--- blog-frontend/src/index.tsx
+++ blog-frontend/src/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
+import EditPost from './components/EditPost';
+import NewPost from './components/NewPost';
+import Home from './pages/Home';
+import Post from './components/Post';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/newPost" element={<NewPost />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/editPost/:id" element={<EditPost />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
