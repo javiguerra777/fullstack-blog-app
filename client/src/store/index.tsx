90d09f99a4f489d0910157b6eb6e2b61de81f709
Javier Guerra <javier.guerra1001@gmail.com>
2023-10-18 15:47:10 -0700

    feat(persist): added functionality to persist redux state to app

--- client/src/store/index.tsx
+++ client/src/store/index.tsx
@@ -1,27 +1,38 @@
 /* eslint-disable implicit-arrow-linebreak */
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import PostSlice from './PostSlice';
 import UserSlice from './UserSlice';
 import CommentSlice from './CommentSlice';
 import categoriesApi from '../common/api/categoriesApi';
 import postsApi from '../common/api/postsApi';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+const rootReducer = combineReducers({
+  user: UserSlice,
+  post: PostSlice,
+  comment: CommentSlice,
+  [categoriesApi.reducerPath]: categoriesApi.reducer,
+  [postsApi.reducerPath]: postsApi.reducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: {
-    post: PostSlice,
-    user: UserSlice,
-    comment: CommentSlice,
-    [categoriesApi.reducerPath]: categoriesApi.reducer,
-    [postsApi.reducerPath]: postsApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       categoriesApi.middleware,
       postsApi.middleware,
     ),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
